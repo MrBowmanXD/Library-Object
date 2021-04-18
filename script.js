b542f0b9b5e2050af76548239ec8a2862341a8e5
Manuel Gonçalves <mjgoncalves.trabalho@gmail.com>
2021-04-18 18:18:28 +0100

    Functional version 1.0.2

--- script.js
+++ script.js
@@ -1,94 +1,32 @@
 "use strict";
 
+// Constant of the document.querySelectors
+const formFields = document.querySelector(".formfields");
+const containerNew = document.querySelector(".container-new");
+const newBookQuery = document.querySelector(".newbook");
+const done = document.querySelector(".done");
+const container = document.querySelector(".container");
+
 let myLibrary = [];
 
-function Book(title, author, pages, read) {
-  this.bookTitle = title;
-  this.bookAuthor = author;
-  this.bookPages = pages;
-  this.read = read;
-  this.info = function () {
-    return `${title} by ${author}, ${pages}, ${read} `;
-  };
-}
-
-const HarryPoter = new Book(
-  "Harry Potter and the Philosopher Stone",
-  "J.K.Rowling",
-  223,
-  "not read yet"
-);
-
-const LordOfTheRings = new Book(
-  "Lord of the Rings",
-  "J.R.R Tolkien",
-  1136,
-  "not read yet"
-);
-
-const ToAGodUnknown = new Book(
-  "To a God Unknown",
-  "Jonh Steinbeck",
-  282,
-  "read it fully"
-);
-
-function addBookToLibrary(variable) {
-  myLibrary.push(variable);
-}
-
-addBookToLibrary(HarryPoter);
-
-addBookToLibrary(LordOfTheRings);
-
-addBookToLibrary(ToAGodUnknown);
-
-function loopArray() {
-  for (let i = 0; i < myLibrary.length; i++) {
-    console.log(myLibrary[i]);
+class Book {
+  constructor(title, author, pages, read) {
+    this.bookTitle = title;
+    this.bookAuthor = author;
+    this.bookPages = pages;
+    this.read = read;
+    this.info = function () {
+      return `${title} by ${author}, ${pages}, ${read} `;
+    };
   }
-}
 
-document.querySelector(".formfields").style.cssText = "display: none;";
+  addBookToLibrary(book) {
+    myLibrary.push(book);
+  }
 
-function showForm() {
-  document.querySelector(".formfields").style.cssText = "display: block;";
-}
-
-document.querySelector(".newbook").addEventListener("click", showForm);
-
-function hideForm(e) {
-  e.preventDefault();
-
-  const title = document.querySelector("#title");
-
-  const titleValue = title.value;
-
-  const author = document.querySelector("#author");
-
-  const authorValue = author.value;
-
-  const pages = document.querySelector("#pages");
-
-  const pagesValue = pages.value;
-
-  const read = document.querySelector("#read");
-
-  const readValue = read.value;
-
-  document.querySelector(".formfields").style.cssText = "display: none;";
-
-  // Add information in the myLibrary Array
-  const newBook = new Book(titleValue, authorValue, pagesValue, readValue);
-  myLibrary.push(newBook);
-
-  loopArray();
-
-  // if box is already created, create a new box
-
-  // to create a new box i need to add html
-  let html;
-  html = `
+  addBook(titleValue, authorValue, pagesValue, readValue) {
+    let html;
+    html = `
   <div class="box" id="display">
   <p>Title</p>
   <div class="titulo">${titleValue}</div>
@@ -98,39 +36,64 @@ function hideForm(e) {
   <div class="paginas">${pagesValue}</div>
   <p>Read(yes or no)</p>
   <div class="lido">${readValue}</div>
-  <button class="delete">Delete</button>
-  <button class="read-status">Read:</button>
+  <button class="delete btn">Delete</button>
+  <button class="read-status btn">Read:</button>
 </div>`;
 
-  document.querySelector(".container").innerHTML += html;
+    container.insertAdjacentHTML("afterbegin", html);
 
-  document.querySelector("#display").classList.remove("display");
-}
+    document.querySelector(".delete").addEventListener("click", function (e) {
+      const btn = e.target.closest(".box");
+      btn.classList.add("display-forever");
+    });
 
-document.querySelector(".done").addEventListener("click", hideForm);
+    document
+      .querySelector(".read-status")
+      .addEventListener("click", function (e) {
+        const btn = e.target.closest(".read-status");
 
-// Toggle delete button
+        if (btn.textContent === "Read:") {
+          btn.textContent = "Read: Yes";
+        } else if (btn.textContent === "Read: Yes") {
+          btn.textContent = "Read: No";
+        } else if (btn.textContent === "Read: No") {
+          btn.textContent = "Read: Yes";
+        }
+      });
+  }
 
-function deleteButton() {
-  document.querySelector(".box").classList.toggle("display-forever");
-}
+  showForm() {
+    containerNew.classList.add("overlay");
+    formFields.style.cssText = "display: block;";
+  }
 
-document.querySelector(".delete").addEventListener("click", deleteButton);
+  hideForm(e) {
+    e.preventDefault();
 
-// Toggle the read status
+    containerNew.classList.remove("overlay");
 
-function readStatus() {
-  if (document.querySelector(".read-status").textContent === "Read:") {
-    document.querySelector(".read-status").textContent = "Read: Yes";
-  } else if (
-    document.querySelector(".read-status").textContent === "Read: Yes"
-  ) {
-    document.querySelector(".read-status").textContent = "Read: No";
-  } else if (
-    document.querySelector(".read-status").textContent === "Read: No"
-  ) {
-    document.querySelector(".read-status").textContent = "Read: Yes";
+    const title = document.querySelector("#title");
+    const titleValue = title.value;
+    const author = document.querySelector("#author");
+    const authorValue = author.value;
+    const pages = document.querySelector("#pages");
+    const pagesValue = pages.value;
+    const read = document.querySelector("#read");
+    const readValue = read.value;
+
+    formFields.style.cssText = "display: none;";
+
+    // Add information in the myLibrary Array
+    const newBook = new Book(titleValue, authorValue, pagesValue, readValue);
+
+    myLibrary.push(newBook);
+
+    Book.prototype.addBook(titleValue, authorValue, pagesValue, readValue);
   }
 }
 
-document.querySelector(".read-status").addEventListener("click", readStatus);
+formFields.style.cssText = "display: none;";
+
+newBookQuery.addEventListener("click", Book.prototype.showForm);
+
+done.addEventListener("click", Book.prototype.hideForm);
